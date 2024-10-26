d3d769b26a1aa49093c4d26ea1ba6a111bcccc31
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-10-26 14:21:22 -0500

    add link to life lessons

--- gen/gen_index.sh
+++ gen/gen_index.sh
@@ -2,6 +2,8 @@
 
 echo -e "---\ntitle: All Stories\n---\n"
 
+echo "You can also see the stories sorted by life lessons [here](/life_lessons.html)\n"
+
 for i in $(find md -maxdepth 1 -name "*.md" -printf '%f\n'); do
   (
     title=$(sed -n 's/^title: //p' "md/$i")
